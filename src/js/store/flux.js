8c9d1e89d06304a7da292bfe5c94d9d4cb9842e9
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-06-10 21:08:43 +0000

    Primer intento

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,91 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+    return {
+        store: {
+            characters: [],
+            planets: [],
+            singleCharacter: {},
+            singleCharacterProperties: {},
+            singlePlanet: {},
+            singlePlanetProperties: {},
+            starships: [],
+            singleStarship: {},
+            singleStarshipProperties: {},
+            favorites: []
+        },
+        actions: {
+            getCharacters: async () => {
+                try {
+                    const res = await fetch(`https://www.swapi.tech/api/people/`);
+                    const data = await res.json();
+                    setStore({ characters: data.results });
+                } catch (error) {
+                    console.error(error);
+                }
+            },
+            getPlanets: async () => {
+                try {
+                    const res = await fetch(`https://www.swapi.tech/api/planets`);
+                    const data = await res.json();
+                    setStore({ planets: data.results });
+                } catch (error) {
+                    console.error(error);
+                }
+            },
+            getCharactersSingle: async (id) => {
+                try {
+                    const res = await fetch(`https://www.swapi.tech/api/people/${id}`);
+                    const data = await res.json();
+                    setStore({ singleCharacter: data.result });
+                    setStore({ singleCharacterProperties: data.result.properties });
+                } catch (error) {
+                    console.error("no hubo conexion en getCharacterSingle", error);
+                }
+            },
+            getPlanetsSingle: async (id) => {
+                try {
+                    const res = await fetch(`https://www.swapi.tech/api/planets/${id}`);
+                    const data = await res.json();
+                    setStore({ singlePlanet: data.result });
+                    setStore({ singlePlanetProperties: data.result.properties });
+                } catch (error) {
+                    console.error("no hubo conexion en getCharacterSingle", error);
+                }
+            },
+            getStarships: async () => {
+                try {
+                    const res = await fetch(`https://www.swapi.tech/api/starships/`);
+                    const data = await res.json();
+                    setStore({ starships: data.results });
+                } catch (error) {
+                    console.error(error);
+                }
+            },
+            getStarshipSingle: async (id) => {
+                try {
+                    const res = await fetch(`https://www.swapi.tech/api/starships/${id}`);
+                    const data = await res.json();
+                    setStore({ singleStarship: data.result });
+                    setStore({ singleStarshipProperties: data.result.properties });
+                } catch (error) {
+                    console.error("no hubo conexion en getCharacterSingle", error);
+                }
+            },
+            getFavorite: (favorite) => {
+                let storeFavorites = getStore().favorites;
+                setStore({ favorites: [...storeFavorites, favorite] });
+            },
+            removeFavorite: (index) => {
+                let storeFavorites = getStore().favorites;
+                storeFavorites.splice(index, 1);
+                setStore({ favorites: storeFavorites });
+            },
+            loadSomeData: () => {
+                getActions().getCharacters();
+                getActions().getPlanets();
+                getActions().getStarships();
+            },
+        }
+    };
 };
 
 export default getState;
---
+++ src/js/views/CharacterDetails.js
@@ -0,0 +1,66 @@
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+const CharacterDetails = () => {
+    const { store, actions } = useContext(Context);
+    const { uid } = useParams();
+
+    useEffect(() => {
+        actions.getCharactersSingle(uid);
+    }, [uid]);
+
+    const character = store.singleCharacter;
+    const properties = store.singleCharacterProperties;
+
+    if (!character) return <div>Loading...</div>;
+
+    return (
+        <div className="container-fluid bg-dark text-light d-flex flex-column justify-content-center" style={{ minHeight: "100vh" }}>
+            <div className="row border-bottom border-2 pb-4 border-danger py-5">
+                <div className="col-md-6">
+                    <img src={`https://starwars-visualguide.com/assets/img/characters/${uid}.jpg`} alt="Character" className="img-fluid rounded" />
+                </div>
+                <div className="col-md-6 text-center d-flex flex-column justify-content-center">
+                    <h1>{properties.name}</h1>
+                    <p>{character.description}</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Asperiores eaque exercitationem numquam architecto tenetur fugit praesentium sunt iste eos,
+                        delectus minima saepe quam impedit obcaecati maiores,
+                        voluptatum cupiditate quibusdam.
+                        Tenetur.
+                    </p>
+                </div>
+            </div>
+            <div className="row py-5">
+                <div className="col">
+                    <p><strong>Name</strong></p>
+                    <p>{properties.name}</p>
+                </div>
+                <div className="col">
+                    <p><strong>Birth Year</strong></p>
+                    <p>{properties.birth_year}</p>
+                </div>
+                <div className="col">
+                    <p><strong>Gender</strong></p>
+                    <p>{properties.gender}</p>
+                </div>
+                <div className="col">
+                    <p><strong>Height</strong></p>
+                    <p>{properties.height}</p>
+                </div>
+                <div className="col">
+                    <p><strong>Skin Color</strong></p>
+                    <p>{properties.skin_color}</p>
+                </div>
+                <div className="col">
+                    <p><strong>Eye Color</strong></p>
+                    <p>{properties.eye_color}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export { CharacterDetails };
